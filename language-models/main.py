6adde0cfe4528b7aed71df6e32a89641d98303be
southPaw <dangargiullo@gmail.com>
2024-02-19 09:31:19 -0500

    Complete the working `preprocess_data()` as one function (need to separate into multiple functions)

--- language-models/main.py
+++ language-models/main.py
@@ -1,35 +1,93 @@
 # v =   vocabulary                  =   set of all words in a language
 
 
-def preprocess_data(training_corpus, testing_corpus):
-    def handle_unknowns(tokens: list, types: dict, include_unknowns: bool):
-        if include_unknowns:
-            for i, phrase in enumerate(tokens):
-                tokens[i] = ['<unk>' if types[w] == 1 and w not in ('<s>', '</s>')
-                             else w for w in phrase
-                             ]
-        else:
-            for i, phrase in enumerate(tokens):
-                tokens[i] = ['' if types[w] == 1 and w not in ('<s>', '</s>')
-                             else w for w in phrase
-                             ]
+def preprocess_data(train_file, test_file, include_unknowns):
+    word_counts = {}
 
-    u = {}  # dict for storing all unique words (types) in the corpus (no repeats)
-    with open(file=training_corpus, mode='r') as f:  # f = the current file being read from/written to
-        n = []  # list for storing all preprocessed words (tokens) in the corpus (has repeats)
-        for c in f:  # c = current line (not processed);     For the current line in the file,
-            c = c.strip().lower()  # strip leading and trailing whitespace in `c` and make `c` all lowercase.
-            if not c.startswith('<s>'):  # If no leading '<s>' in this line,
-                c = '<s> ' + c  # add it.
-            if not c.endswith('</s>'):  # If no trailing '</s>' in this line,
-                c = c + ' </s>'  # add it.
-            p = c.split()  # p = preprocessed `c`
-            n.append(p)  # add the now preprocessed current line's tokens to `n`
-            for w in p:  # w = a single word;    For each word in the current processed line,
-                print()     # NEED TO FINISH
-                # u[w] = u.get(w, 0) + 1  # update the counts of each word in `p` in the list `n`.
+    with open(file=train_file, mode='r', encoding='utf-8') as file:
+        train_sentences = []
+        for line in file:
+            line = line.strip().lower()
+            # Ensure <s> and </s> are not already present:
+            if not line.startswith('<s>'):
+                line = '<s> ' + line
+            if not line.endswith('</s>'):
+                line = line + ' </s>'
+            words = line.split()
+            train_sentences.append(words)
+            for word in words:
+                word_counts[word] = word_counts.get(word, 0) + 1
 
-    handle_unknowns(tokens=n, types=u, include_unknowns=True)
+    if include_unknowns:
+        # Process the words in train.txt
+        for i, sentence in enumerate(train_sentences):
+            train_sentences[i] = ['<unk>' if word_counts[word] == 1 and word not in ('<s>', '</s>') else word for word in sentence]
+    else:
+        for i, sentence in enumerate(train_sentences):
+            train_sentences[i] = ['' if word_counts[word] == 1 and word not in ('<s>', '</s>') else word for word in sentence]
+
+    # Write the processed sentences back to train.txt
+    with open(file=train_file, mode='w', encoding='utf-8') as file:
+        for sentence in train_sentences:
+            file.write(' '.join(sentence) + '\n')
+
+    # Process test file
+    with open(file=test_file, mode='r', encoding='utf-8') as file:
+        test_sentences = []
+        for line in file:
+            line = line.strip().lower()
+            if not line.startswith('<s>'):
+                line = '<s> ' + line
+            if not line.endswith('</s>'):
+                line = line + ' </s>'
+            words = line.split()
+            test_sentences.append(words)
+
+    if include_unknowns:
+        for i, sentence in enumerate(test_sentences):
+            test_sentences[i] = ['<unk>' if word not in word_counts or word_counts[word] == 1 else word for word in sentence]
+    else:
+        for i, sentence in enumerate(test_sentences):
+            test_sentences[i] = [word for word in sentence]
+
+    with open(file=test_file, mode='w', encoding='utf-8') as file:
+        for sentence in test_sentences:
+            file.write(' '.join(sentence) + '\n')
+
+
+def map_singletons(train_file, test_file):
+    word_counts = {}  # Dictionary to count the occurrences of each word in train.txt
+
+    # Process train file to get word counts
+    with open(file=train_file, mode='r', encoding='utf-8') as file:
+        for line in file:
+            words = line.strip().split()
+            for word in words:
+                word_counts[word] = word_counts.get(word, 0) + 1
+
+    # Process train file again to replace singletons with <unk>
+    with open(file=train_file, mode='r', encoding='utf-8') as file:
+        train_sentences = [line.strip() for line in file]
+        for i, sentence in enumerate(train_sentences):
+            words = sentence.split()
+            train_sentences[i] = ' '.join(['<unk>' if word_counts[word] == 1 else word for word in words])
+
+    # Write the modified train sentences back to train.txt
+    with open(file=train_file, mode='w', encoding='utf-8') as file:
+        for sentence in train_sentences:
+            file.write(sentence + '\n')
+
+    # Process test file to map unobserved words to <unk>
+    with open(file=test_file, mode='r', encoding='utf-8') as file:
+        test_sentences = [line.strip() for line in file]
+        for i, sentence in enumerate(test_sentences):
+            words = sentence.split()
+            test_sentences[i] = ' '.join(['<unk>' if word not in word_counts else word for word in words])
+
+    # Write the modified test sentences back to test.tx
+    with open(file=test_file, mode='w', encoding='utf-8') as file:
+        for sentence in test_sentences:
+            file.write(sentence + '\n')
 
 
 if __name__ == '__main__':
