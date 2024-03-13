d51c4f63758175c0c120782f0f2ed7adf92f5a3d
southPaw <dangargiullo@gmail.com>
2024-03-13 13:31:00 -0400

    Condense `normalize` function, change notation, remove extraneous variables and word count from function

--- language-models/main.py
+++ language-models/main.py
@@ -1,41 +1,36 @@
-# v =   vocabulary  =   set of all word types in a language
-
 def normalize(corpora: iter):
     """
-    u   =   word types:     the set of all unique words in the corpus (has no repeats)
-    n   =   word tokens:    the set of ALL words in the corpus (has repeats)
-    w   =   a single word token
-    f   =   the current `corpus` (text file) being normalized
-    c   =   the current line in `f`
-    p   =   normalized `c`
+    T   =   the current corpus (text file) being normalized
+    S   =   sentences:  the set of all sentences in the corpus
+    s   =   sentence:   a single, non-normalized sentence
+    n   =   a single, normalized sentence s
+    w   =   word:   a single word token
 
     ** NOTE: This function assumes that the text file(s) in `corpora` has/have already been tokenized **
 
     Takes a list of one or more text files (corpora) and normalizes them:
     All text becomes lower-case, and '<s>' and '</s>' are added as leading and trailing sentinels, respectively, to each line.
-    Each of the normalized text files in `corpora` are then written back to their respective files.
+    Each of the normalized text files in `corpora` are then written back to files of the same name and put in the `input_data/processed/` directory.
 
     :param corpora: an iterable set of text files (corpora) to be normalized
     :return: None
     """
     for corpus in corpora:
-        u = {}  # u: dict
-        with open(file=corpus, mode='r', encoding='utf-8') as f:  # read in
-            n = []  # n: list
-            for c in f:
-                c = c.strip().lower()
-                if not c.startswith('<s>'):
-                    c = '<s> ' + c
-                if not c.endswith('</s>'):
-                    c = c + ' </s>'
-                p = c.split()
-                n.append(p)
-                for w_ in p:
-                    u[w_] = u.get(w_, 0) + 1
-        with open(file=corpus, mode='w', encoding='utf-8') as f:  # write back
-            for w in n:
-                f.write(' '.join(w) + '\n')
+        with open(file=corpus, mode='r', encoding='utf-8') as T:  # read in from file
+            S = []  # S: list
+            for s in T:
+                s = s.strip().lower()
+                if not s.startswith('<s>'):
+                    s = '<s> ' + s
+                if not s.endswith('</s>'):
+                    s = s + ' </s>'
+                n = s.split()  # .split() returns a list of substrings (words) in `s`
+                S.append(n)
+        corpus_processed = corpus.replace('raw', 'processed')
+        with open(file=corpus_processed, mode='w', encoding='utf-8') as T:  # write to new file
+            for w in S:
+                T.write(' '.join(w) + '\n')
 
 
 if __name__ == '__main__':
-    normalize(['input_data/raw/test-Fall2023.txt', 'input_data/raw/train-Fall2023.txt'])
+    normalize(['input_data/raw/train.txt', 'input_data/raw/test.txt'])
